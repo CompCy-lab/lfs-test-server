06215a34c53a91380734a36afbf9aae7bcad6bff
HaidYi <haidyi@cs.unc.edu>
2023-10-30 11:44:12 -0400

    add entrypoint for docker

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+LFS_LISTEN="tcp://:9999"
+LFS_HOST="127.0.0.1:9999"
+LFS_METADB="/data/lfs.db"
+LFS_CONTENTPATH="/data/lfs.content"
+LFS_ADMINUSER="xxxxxx"
+LFS_ADMINPASS="xxxxxx"
+#LFS_CERT="mine.crt"
+#LFS_KEY="mine.key"
+
+export LFS_LISTEN LFS_HOST LFS_METADB LFS_CONTENTPATH LFS_ADMINUSER LFS_ADMINPASS
+
+./lfs-test-server
